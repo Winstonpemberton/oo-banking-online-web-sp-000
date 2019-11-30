9d806f037b719b441cb4b543005a34004de44197
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-30 16:42:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,22 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status = "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance"
-    end
+    # if @sender.balance > @amount && @status = "pending"
+    #   @sender.balance -= @amount
+    #   @receiver.balance += @amount
+    #   @status = "complete"
+    # else
+    #   @status = "rejected"
+    #   "Transaction rejected. Please check your account balance"
+    # end
+    if @sender.balance > @amount && @status == "pending"
+   @sender.balance -= @amount
+   @receiver.balance += @amount
+   @status = "complete"
+ else
+   @status = "rejected"
+   return "Transaction rejected. Please check your account balance."
+ end
   end
 
 
