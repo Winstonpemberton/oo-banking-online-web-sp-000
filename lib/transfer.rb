8203f56497d240ea02b242c32270df0302afc9da
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-30 17:01:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,16 +14,26 @@ class Transfer
     sender.valid? && receiver.valid? ? true : false
   end
 
+  # def execute_transaction
+  #   if @status = "pending" && sender.balance > @amount
+  #     sender.balance -= @amount
+  #     receiver.balance += @amount
+  #     @status = "complete"
+  #   elsif @status = "pending" && sender.balance < @amount
+  #     binding.pry
+  #     @status = "rejected"
+  #     return "Transaction rejected. Please check your account balance"
+  #   end
   def execute_transaction
-    if @status = "pending" && sender.balance > @amount
-      sender.balance -= @amount
-      receiver.balance += @amount
-      @status = "complete"
-    elsif @status = "pending" && sender.balance < @amount
-      binding.pry
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance"
-    end
+  if @sender.balance > @amount && @status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  else
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
+end
   end
 
 
