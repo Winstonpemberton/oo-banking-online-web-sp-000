8d80a7410eadfd9e949659390e265419b2c682f5
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-30 16:17:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,11 +18,13 @@ class BankAccount
   end
 
   def valid?
-    if balance > 0 && status == "open"
-      true
-    else
-      false
-    end
+    # if balance > 0 && status == "open"
+    #   true
+    # else
+    #   false
+    # end
+
+    balance > 0 && status == "open" ? true : false
   end
 
 
