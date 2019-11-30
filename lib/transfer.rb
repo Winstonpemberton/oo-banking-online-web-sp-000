1bae427d9956bd4dccc9514757977fae8b317b16
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-30 16:23:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+
+  def initialize(sender, reciever, transfer_amount)
+    @sender = sender
+    @reciever = reciever
+    @transfer_amount = transfer_amount
+    @status = "pending"
+
+  end
+
+  
 end
